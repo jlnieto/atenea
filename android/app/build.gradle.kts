f077f894de0f4b85e8c45dd77cc92f71858f4aae
Jose <info@yudri.es>
2026-05-25 00:16:50 +0000

    Use WA0009 as Atenea app icon

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.atenea.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 123
-        versionName = "0.5.90"
+        versionCode = 124
+        versionName = "0.5.91"
 
         val ateneaApiBaseUrl = providers.gradleProperty("ATENEA_API_BASE_URL")
             .orElse("https://atenea.yudri.es")
